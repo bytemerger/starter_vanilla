eddab30d37699959a2daf612d1605a236bb93c6f
francis <onyishif@gmail.com>
2020-06-16 17:13:49 +0100

    export the hompage template

--- src/components/home.js
+++ src/components/home.js
@@ -2,15 +2,17 @@ import home from '../views/home.njk';
 import {getBoards} from "../api/Boards";
 import {view} from "../components/view"
 import img from "../assets/image/blank-cover-456424abf2806d03948a4181f9d90c6e.jpg"
-getBoards().then(
-    data => boards(data)
-)
-const boards=(data)=> {
-    document.getElementById('app').innerHTML = home.render({result: data, img})
-    document.querySelectorAll(".board-button").forEach((button)=>(
-        button.addEventListener('click',function () {
-            view(button.dataset.id)
-        }))
+export function homepage() {
+    getBoards().then(
+        data => boards(data)
     )
+    const boards = (data) => {
+        document.getElementById('app').innerHTML = home.render({result: data, img})
+        document.querySelectorAll(".board-button").forEach((button) => (
+            button.addEventListener('click', function () {
+                view(button.dataset.id)
+            }))
+        )
+    }
 }
 //view(image.dataset.id);
