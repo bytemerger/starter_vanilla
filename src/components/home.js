cf46cf74cca1759bd0609f411864329c4c83b184
francis <onyishif@gmail.com>
2020-06-16 16:39:29 +0100

    correction to enable transition to view page

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,16 @@
 import home from '../views/home.njk';
 import {getBoards} from "../api/Boards";
+import {view} from "../components/view"
 import img from "../assets/image/blank-cover-456424abf2806d03948a4181f9d90c6e.jpg"
 getBoards().then(
     data => boards(data)
 )
-const boards=(data)=>(
-    document.getElementById('app').innerHTML=home.render({result:data,img})
-)
+const boards=(data)=> {
+    document.getElementById('app').innerHTML = home.render({result: data, img})
+    document.querySelectorAll(".board-button").forEach((button)=>(
+        button.addEventListener('click',function () {
+            view(button.dataset.id)
+        }))
+    )
+}
+//view(image.dataset.id);
